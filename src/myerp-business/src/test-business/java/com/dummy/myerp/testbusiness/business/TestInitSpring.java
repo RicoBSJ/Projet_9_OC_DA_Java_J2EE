8b3babc922ed1d064388b7a5598236af22deb8dd
RicoBSJ <aubrun.eric@free.fr>
2021-06-17 08:46:50 +0200

    NewCommit

--- src/myerp-business/src/test-business/java/com/dummy/myerp/testbusiness/business/TestInitSpring.java
+++ src/myerp-business/src/test-business/java/com/dummy/myerp/testbusiness/business/TestInitSpring.java
@@ -1,9 +1,7 @@
 package com.dummy.myerp.testbusiness.business;
 
 import org.junit.Test;
-
 import static org.junit.Assert.assertNotNull;
-
 
 /**
  * Classe de test de l'initialisation du contexte Spring
