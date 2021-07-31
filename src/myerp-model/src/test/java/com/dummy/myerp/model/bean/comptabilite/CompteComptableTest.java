4f715f1ba74eddc10aefb5f413863c5ef8bed139
RicoBSJ <44947358+RicoBSJ@users.noreply.github.com>
2021-07-31 10:27:58 +0200

    addGetByCodeTestAndGetByNumeroTest

---
+++ src/myerp-model/src/test/java/com/dummy/myerp/model/bean/comptabilite/CompteComptableTest.java
@@ -0,0 +1,18 @@
+package com.dummy.myerp.model.bean.comptabilite;
+
+import org.junit.Test;
+
+public class CompteComptableTest {
+
+    @Test
+    public void getByNumeroTest() {
+        /*CompteComptable vRetour = null;
+        for (CompteComptable vBean : pList) {
+            if (vBean != null && Objects.equals(vBean.getNumero(), pNumero)) {
+                vRetour = vBean;
+                break;
+            }
+        }
+        return null;*/
+    }
+}
---
+++ src/myerp-model/src/test/java/com/dummy/myerp/model/bean/comptabilite/JournalComptableTest.java
@@ -0,0 +1,21 @@
+package com.dummy.myerp.model.bean.comptabilite;
+
+import org.junit.Test;
+
+import java.util.List;
+import java.util.Objects;
+
+public class JournalComptableTest {
+
+    @Test
+    public void getByCodeTest() {
+        /*JournalComptable vRetour = null;
+        for (JournalComptable vBean : pList) {
+            if (vBean != null && Objects.equals(vBean.getCode(), pCode)) {
+                vRetour = vBean;
+                break;
+            }
+        }
+        return vRetour;*/
+    }
+}
