dba09a87a42173781030d42a32e2bcfb2c7aeff1
RicoBSJ <44947358+RicoBSJ@users.noreply.github.com>
2021-08-07 19:05:13 +0200

    keepOnGetBetterCoverageCodeV4

--- src/myerp-model/src/test/java/com/dummy/myerp/model/bean/comptabilite/CompteComptableTest.java
+++ src/myerp-model/src/test/java/com/dummy/myerp/model/bean/comptabilite/CompteComptableTest.java
@@ -9,7 +9,7 @@ import java.util.List;
 public class CompteComptableTest {
 
     @Test
-    public void getByNumero() {
+    public void getByNumeroTest() {
 
         CompteComptable compteComptable1 = new CompteComptable();
         compteComptable1.setNumero(401);
@@ -19,11 +19,31 @@ public class CompteComptableTest {
         compteComptable3.setNumero(401);
         CompteComptable compteComptable4 = new CompteComptable();
         compteComptable4.setNumero(4456);
-        List<Integer> list = new ArrayList<>();
-        list.add(compteComptable1.getNumero());
-        list.add(compteComptable2.getNumero());
-        list.add(compteComptable3.getNumero());
-        list.add(compteComptable4.getNumero());
-        Assertions.assertThat(list.contains(4454)).isFalse();
+        List<CompteComptable> list = new ArrayList<>();
+        list.add(compteComptable1);
+        list.add(compteComptable2);
+        list.add(compteComptable3);
+        list.add(compteComptable4);
+        CompteComptable vRetour = null;
+        for (CompteComptable vBean : list) {
+            if (vBean != null && vBean.getNumero().equals(compteComptable4.getNumero())) {
+                vRetour = vBean;
+                break;
+            }
+        }
+        Assertions.assertThat(vRetour == compteComptable4).isTrue();
     }
+
+    /*
+    public static CompteComptable getByNumero(List<? extends CompteComptable> pList, Integer pNumero) {
+        CompteComptable vRetour = null;
+        for (CompteComptable vBean : pList) {
+            if (vBean != null && Objects.equals(vBean.getNumero(), pNumero)) {
+                vRetour = vBean;
+                break;
+            }
+        }
+        return vRetour;
+    }
+     */
 }
